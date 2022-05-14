3a9a27ea6957a62f0b7c7672e48396ea96c23e22
cmaxNZ <60564634+cmaxNZ@users.noreply.github.com>
2022-05-14 21:51:20 +1200

    adjust styling

--- src/components/gameOver.js
+++ src/components/gameOver.js
@@ -6,10 +6,10 @@ function GameOver() {
      const { gameOver, word, currentAttempt, initGame } = useContext(AppContext);
 	return (
     <div className="gameOver">
-        <h3>{gameOver.won ? "You won!" : "You lost!"}</h3>
-        <h1>Wordle: {word}</h1>
+        <h1>{gameOver.won ? "You won!" : "You lost!"}</h1>
+        <h2>Wordle: {word}</h2>
         {gameOver.won && (<h3>You guessed in {currentAttempt.attempt + 1} tries</h3>)}
-        <h1 onClick={initGame}>Play again?</h1>
+        <h2 onClick={initGame}>Play again?</h2>
     </div>
     );
 }
